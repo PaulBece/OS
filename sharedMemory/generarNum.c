42c586455a08b032ee4fa3246eb5e861ee21eb1b
Paul Becerra <paulbecerracs@gmail.com>
2024-06-24 17:59:28 -0500

    fix semaphore

--- sharedMemory/generarNum.c
+++ sharedMemory/generarNum.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,95 +5,72 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
-#include <errno.h>
 #include <sys/shm.h>
 
 #define KEY 0x1111
-#define SHMSZ     27
+#define SHMSZ 27
 
-union semun
-{
+union semun {
     int val;
     struct semid_ds *buf;
     unsigned short *array;
 };
 
-
-
 struct sembuf p = {0, -1, SEM_UNDO};
 struct sembuf v = {0, +1, SEM_UNDO};
 
-main()
-{
-    int shmid;
-    key_t key;
-    char *shm;
+int main() {
+    int shmid, shmid2, semid;
+    key_t key = 5678, key2 = 8765;
+    char *shm, *shm2;
+    char last[100] = {0};
+    int num = 100, c = 0;
 
-    char last[100];
-    int num = 100;
-    int c=0;
-
-    /*
-     * We need to get the segment named
-     * "5678", created by the server.
-     */
-    key = 5678;
-
-    /*
-     * Locate the segment.
-     */
     if ((shmid = shmget(key, SHMSZ, 0666)) < 0) {
         perror("shmget");
         exit(1);
     }
-
-    /*
-     * Now we attach the segment to our data space.
-     */
+    if ((shmid2 = shmget(key2, SHMSZ, 0666)) < 0) {
+        perror("shmget");
+        exit(1);
+    }
     if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) {
         perror("shmat");
         exit(1);
     }
-    *last=NULL;
-    sprintf(shm,"%d\0",num);
+    if ((shm2 = shmat(shmid2, NULL, 0)) == (char *) -1) {
+        perror("shmat");
+        exit(1);
+    }
 
-    int id = semget(KEY, 1, 0666 | IPC_CREAT);
-    if (id < 0)
-    {
+    semid = semget(KEY, 1, 0666 | IPC_CREAT);
+    if (semid < 0) {
         perror("semget");
-        exit(11);
-    }
-    union semun u;
-    u.val = 1;
-    if (semctl(id, 0, SETVAL, u) < 0)
-    {
-        perror("semctl");
-        exit(12);
+        exit(1);
     }
 
+    sprintf(shm, "%d\0", num);
 
-    while (c<100){
-        if (strcmp(last,shm)!=0){
-
-            if (semop(id, &p, 1) < 0) {
+    while (c < 100) {
+        if (semop(semid, &p, 1) < 0) {
             perror("semop p");
-            exit(13);
-            }
-            sprintf(shm,"%d\0",num++);
-            strncpy(last,shm,20);
-            last[20]='\0';
-            printf("%s\n",shm);
+            exit(1);
+        }
+
+        if (*shm2 && strcmp(last, shm) != 0) {
+            sprintf(shm, "%d\0", num++);
+            strncpy(last, shm, 20);
+            last[20] = '\0';
+            printf("%s\n", shm);
             c++;
-            
-            if (semop(id, &v, 1) < 0)
-            {
-                perror("semop p");
-                exit(14);
-            }
+            *shm2 = '\0';
+        }
+
+        if (semop(semid, &v, 1) < 0) {
+            perror("semop v");
+            exit(1);
         }
     }
-
-    
 
     exit(0);
 }
--- sharedMemory/processPrint.c
+++ sharedMemory/processPrint.c
@@ -2,55 +2,43 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define SHMSZ     27
+#define SHMSZ 27
 
-main()
-{
-    char c;
-    int shmid;
-    key_t key;
-    char *shm;
-    char last[100];
+int main() {
+    int shmid, shmid2;
+    key_t key = 5678, key2 = 8765;
+    char *shm, *shm2;
+    int c = 0;
 
-    /*
-     * We'll name our shared memory segment
-     * "5678".
-     */
-    key = 5678;
-
-    /*
-     * Create the segment.
-     */
     if ((shmid = shmget(key, SHMSZ, IPC_CREAT | 0666)) < 0) {
         perror("shmget");
         exit(1);
     }
-
-    /*
-     * Now we attach the segment to our data space.
-     */
+    if ((shmid2 = shmget(key2, SHMSZ, IPC_CREAT | 0666)) < 0) {
+        perror("shmget");
+        exit(1);
+    }
     if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) {
         perror("shmat");
         exit(1);
     }
+    if ((shm2 = shmat(shmid2, NULL, 0)) == (char *) -1) {
+        perror("shmat");
+        exit(1);
+    }
 
-    
+    *shm2 = '1';
 
-    /*
-     * Now put some things into the memory for the
-     * other process to read.
-     */
-
-    
-    *last = NULL;
-
-    while (1){
-        if (strcmp(shm,last)!=0){
-            strncpy(last,shm,20);
-            last[20]='\0';
-            printf("%s\n",shm);
+    while (c < 200) {
+        if (!(*shm2)) {
+            printf("%s\n", shm);
+            c++;
+            *shm2 = '1';
         }
     }
+
     exit(0);
 }
