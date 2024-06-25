959397a457988ee06bd9742669addc122b703a3e
Paul Becerra <paulbecerracs@gmail.com>
2024-06-24 23:07:48 -0500

    fix semaphore

--- sharedMemory/processPrint.c
+++ sharedMemory/processPrint.c
@@ -1,18 +1,49 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <errno.h>
+#include <sys/shm.h>
 
-#define SHMSZ 27
+#define KEY 0x1111
+#define SHMSZ     27
 
-int main() {
-    int shmid, shmid2;
-    key_t key = 5678, key2 = 8765;
-    char *shm, *shm2;
-    int c = 0;
+union semun
+{
+    int val;
+    struct semid_ds *buf;
+    unsigned short *array;
+};
 
+
+
+struct sembuf p = {0, -1, SEM_UNDO};
+struct sembuf v = {0, +1, SEM_UNDO};
+
+int main()
+{
+    int shmid;
+    key_t key;
+    char *shm;
+
+    int shmid2;
+    key_t key2;
+    char *shm2;
+
+    char last[100];
+
+    /*
+     * We'll name our shared memory segment
+     * "5678".
+     */
+    key = 5678;
+    key2 = 8765;
+    /*
+     * Create the segment.
+     */
     if ((shmid = shmget(key, SHMSZ, IPC_CREAT | 0666)) < 0) {
         perror("shmget");
         exit(1);
@@ -21,24 +52,58 @@ int main() {
         perror("shmget");
         exit(1);
     }
+    /*
+     * Now we attach the segment to our data space.
+     */
     if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) {
         perror("shmat");
         exit(1);
     }
+
     if ((shm2 = shmat(shmid2, NULL, 0)) == (char *) -1) {
         perror("shmat");
         exit(1);
     }
 
-    *shm2 = '1';
+    /*
+     * Now put some things into the memory for the
+     * other process to read.
+     */
 
-    while (c < 200) {
-        if (!(*shm2)) {
-            printf("%s\n", shm);
+    
+    *last = '\0';
+    *shm2='1';
+
+    int id = semget(KEY, 1, 0666 | IPC_CREAT);
+    if (id < 0)
+    {
+        perror("semget");
+        exit(11);
+    }
+    union semun u;
+    u.val = 1;
+    if (semctl(id, 0, SETVAL, u) < 0)
+    {
+        perror("semctl");
+        exit(12);
+    }
+    
+    int c=0;
+    while (c<200){
+        if (semop(id, &p, 1) < 0) {
+            perror("semop p");
+            exit(13);
+        }
+        if (!(*shm2)){
+            printf("%s\n",shm);
             c++;
-            *shm2 = '1';
+            *shm2='1';
+        }
+        if (semop(id, &v, 1) < 0)
+            {
+            perror("semop p");
+            exit(14);
         }
     }
-
     exit(0);
 }
--- sharedMemory/producto.c
+++ sharedMemory/producto.c
@@ -5,27 +5,47 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include <errno.h>
 #include <sys/shm.h>
 
 #define KEY 0x1111
-#define SHMSZ 27
+#define SHMSZ     27
 
-union semun {
+union semun
+{
     int val;
     struct semid_ds *buf;
     unsigned short *array;
 };
 
+
+
 struct sembuf p = {0, -1, SEM_UNDO};
 struct sembuf v = {0, +1, SEM_UNDO};
 
-int main() {
-    int shmid, shmid2, semid;
-    key_t key = 5678, key2 = 8765;
-    char *shm, *shm2;
-    char last[100] = {0};
-    int num, c = 0;
+int main()
+{
+    int shmid;
+    key_t key;
+    char *shm;
 
+    int shmid2;
+    key_t key2;
+    char *shm2;
+
+    char last[100];
+    int num;
+    int c=0;
+
+    /*
+     * We need to get the segment named
+     * "5678", created by the server.
+     */
+    key = 5678;
+    key2 = 8765;
+    /*
+     * Locate the segment.
+     */
     if ((shmid = shmget(key, SHMSZ, 0666)) < 0) {
         perror("shmget");
         exit(1);
@@ -34,6 +54,9 @@ int main() {
         perror("shmget");
         exit(1);
     }
+    /*
+     * Now we attach the segment to our data space.
+     */
     if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) {
         perror("shmat");
         exit(1);
@@ -42,34 +65,36 @@ int main() {
         perror("shmat");
         exit(1);
     }
+    *last='\0';
 
-    semid = semget(KEY, 1, 0666);
-    if (semid < 0) {
+    int id = semget(KEY, 1, 0666 | IPC_CREAT);
+    if (id < 0)
+    {
         perror("semget");
-        exit(1);
+        exit(11);
     }
 
-    while (c < 100) {
-        if (semop(semid, &p, 1) < 0) {
+    while (c<100){
+
+        if (semop(id, &p, 1) < 0) {
             perror("semop p");
-            exit(1);
+            exit(13);
         }
-
-        if (*shm2 && strcmp(last, shm) != 0) {
-            num = atoi(shm);
-            sprintf(shm, "%d,%d\0", num, num * 2);
-            strncpy(last, shm, 20);
-            last[20] = '\0';
-            printf("%s\n", shm);
+        if (*shm2 && strcmp(last,shm)!=0){
+            num=atoi(shm);
+            sprintf(shm,"%d,%d\0",num,num*2);
+            strncpy(last,shm,20);
+            last[20]='\0';
+            printf("%s\n",shm);
             c++;
-            *shm2 = '\0';
+            *shm2='\0';        
+            
         }
-
-        if (semop(semid, &v, 1) < 0) {
-            perror("semop v");
-            exit(1);
+        if (semop(id, &v, 1) < 0)
+            {
+                perror("semop p");
+                exit(14);
         }
     }
-
     exit(0);
 }
